91977fcb9d72d2a294e813125ffc3b08215a17cc
marklutes <lutes319@outlook.com>
2020-03-03 21:29:46 +0000

    COmmit 4 fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,10 +11,12 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
   render(){
+    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
     return(
     <div>
       <QuizQuestion quiz_question={ quizData.quiz_questions[this.state.quiz_position - 1] } />
-      <QuizEnd />
+      { isQuizEnd ? <QuizEnd /> : <div/> }
+
     </div>
   )
 }
