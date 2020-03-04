d8be17c748d680f3f657e4cbd4507c75be5175f5
marklutes <lutes319@outlook.com>
2020-03-04 17:37:39 +0000

    Commit for section 7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,18 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { incorrectAnswer: false };
+  }
+
   handleClick(buttonText){
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler()
+      this.setState({ incorrectAnswer: false })
+    }
+    else{
+      this.setState({ incorrectAnswer: true })
     }
   }
 
@@ -20,6 +29,7 @@ class QuizQuestion extends Component {
           {this.props.quiz_question.answer_options.map((answer_option, index) => <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)}/>)}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
       </main>
     )
   }
